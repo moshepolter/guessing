fce282792fd8bb6a959acccbb5d698a8d0304894
moshe polter <44886334+moshepolter@users.noreply.github.com>
2019-04-29 15:50:21 -0700

    updated code

--- gj.js
+++ gj.js
@@ -1,11 +1,12 @@
-const ranN = Math.floor(Math.random() * 30);
+const ranN = Math.floor(Math.random() * 29 + 1);
 //random number
 let count = 0;
 const g = () =>{
 	//function gor guess
  	count += 1;
-	if(count > 5){
-		alert('to  many tries');
+ 	let attempt = 5 - count;
+	if(count == 6 || attempt == -1){
+		document.getElementById("displayNumber").innerHTML = `Too many tries the answer is ${ranN}`
 		//how many clicks if more than 5 run this code
 	}
 	//if less than 5 continue running this code
@@ -13,15 +14,15 @@ const g = () =>{
 		const numP = document.getElementById("#").value;
 	   if(numP < ranN){
       //if you guessed to high run this code
-      alert(`the number is higher, Attempt: ${count} you have ${5 - count}, attempts left`)
+      document.getElementById("displayNumber").innerHTML = `The number is higher. You have ${5 - count} attempts left`;
     }
     else if (numP > ranN) {
       //if the number you guessed is to low run this code
-      alert(`the number is lower, Attempt: ${count}, you have ${5 - count} attempts left`)
+      document.getElementById("displayNumber").innerHTML = `The number is lower, You have ${5 - count} attempts left`
     }
     else if(numP == ranN){
       //if you guessed the right number run this code
-      alert(`You Guessed The Right Number on attempt: ${count}. thank you for playing`);
+      document.getElementById("displayNumber").innerHTML = `You Guessed The Right Number on attempt: ${count}. Thank You for playing`
     }
 		//after first guess local storage says the number uncomment line below
 		
