bf6f154ed31cd2171acb39612bb4f2ce9c2b9754
moshe polter <44886334+moshepolter@users.noreply.github.com>
2019-05-14 13:32:39 -0700

    made 5th round included

--- gj.js
+++ gj.js
@@ -4,7 +4,17 @@ let count = 0;
 const g = () =>{
 	//function gor guess
 	//if less than 5 continue running this code
+	count += 1;
 		const numP = document.getElementById("#").value;
+		if (numP > 30 || numP < 1){
+			if (count > 4){
+				document.getElementById("displayNumber").innerHTML = `Too many tries the answer is ${ranN}`
+			}
+			else{
+			document.getElementById("displayNumber").innerHTML = `The number is between 1 and 30 You have ${5 - count} attemts left`;
+		}
+		}
+		else{
 	  if(numP < ranN){
       //if you guessed to high run this code
       document.getElementById("displayNumber").innerHTML = `The number is higher. You have ${5 - count} attempts left`;
@@ -18,11 +28,11 @@ const g = () =>{
       document.getElementById("displayNumber").innerHTML = `You Guessed The Right Number on attempt: ${count}. Thank You for playing`
     }
 		//after first guess local storage says the number uncomment line below
-	if(count > 5){
+	if(count > 4){
 		document.getElementById("displayNumber").innerHTML = `Too many tries the answer is ${ranN}`
 		//how many clicks if more than 5 run this code
 	}
-	count += 1;
+}
 		
 		window.localStorage.setItem('number', ranN);
 		console.log(window.localStorage.getItem('number'));	
