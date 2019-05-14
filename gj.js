f2213224d08d1c261ce2d298689ea33d8bc87a13
moshe polter <44886334+moshepolter@users.noreply.github.com>
2019-05-14 16:37:57 -0700

    made it work

--- gj.js
+++ gj.js
@@ -4,17 +4,23 @@ let count = 0;
 const g = () =>{
 	//function gor guess
 	//if less than 5 continue running this code
-	count += 1;
 		const numP = document.getElementById("#").value;
-		if (numP > 30 || numP < 1){
-			if (count > 4){
-				document.getElementById("displayNumber").innerHTML = `Too many tries the answer is ${ranN}`
-			}
-			else{
-			document.getElementById("displayNumber").innerHTML = `The number is between 1 and 30 You have ${5 - count} attemts left`;
+		if (numP <= 0 || numP > 30){
+			document.getElementById("displayNumber").innerHTML = `You must enter a number 1 - 30`
 		}
-		}
+		    else if(numP == ranN){
+      //if you guessed the right number run this code
+      document.getElementById("displayNumber").innerHTML = `You Guessed The Right Number on attempt: ${count + 1}. Thank You for playing`
+      setTimeout(function() {location.reload()}, 2000);
+    }
 		else{
+		count += 1;
+			if(count > 4){
+		document.getElementById("displayNumber").innerHTML = `Too many tries the answer is ${ranN}`;
+		setTimeout(function() {location.reload()}, 2000);
+		//how many clicks if more than 5 run this code
+	}
+	else{
 	  if(numP < ranN){
       //if you guessed to high run this code
       document.getElementById("displayNumber").innerHTML = `The number is higher. You have ${5 - count} attempts left`;
@@ -23,19 +29,14 @@ const g = () =>{
       //if the number you guessed is to low run this code
       document.getElementById("displayNumber").innerHTML = `The number is lower, You have ${5 - count} attempts left`
     }
-    else if(numP == ranN){
-      //if you guessed the right number run this code
-      document.getElementById("displayNumber").innerHTML = `You Guessed The Right Number on attempt: ${count}. Thank You for playing`
-    }
 		//after first guess local storage says the number uncomment line below
-	if(count > 4){
-		document.getElementById("displayNumber").innerHTML = `Too many tries the answer is ${ranN}`
-		//how many clicks if more than 5 run this code
-	}
-}
 		
 		window.localStorage.setItem('number', ranN);
 		console.log(window.localStorage.getItem('number'));	
-		
+		}	
+	}
+}
+const r = () =>{
+	location.reload()
 }
 
